a4357ef20f20b48066976bf2e08a3676a55b741b
emily.c.ritter <emily.c.ritter@gmail.com>
2016-02-16 19:32:13 -0600

    Add sort ability to second table.

--- data_parser.rb
+++ data_parser.rb
@@ -12,7 +12,6 @@ def parse_data_cont file_name, rows
   CSV.foreach(file_name, headers: true) do |row|
     shipment = row.to_hash
 		shipment["Money"] = shipment["Money"].to_f
-		shipment["Money"] = shipment["Money"] / 1.000 if shipment["Money"] > 0
 		shipment["Crates"] = shipment["Crates"].to_f
 		rows << shipment
   end
@@ -35,8 +34,19 @@ def add_bonus ary_of_hsh
   ary_of_hsh.each do |hsh|
     hsh.merge!({"Bonus"=>"#{hsh["Money"]*0.1}"})
 		hsh["Bonus"] = hsh["Bonus"].to_f
-		hsh["Bonus"] = hsh["Bonus"] / 1.000 if hsh["Bonus"] > 0
   end
+end
+
+def formatted_number(n)
+	a,b = sprintf("%0.2f", n).split('.')
+	a.gsub!(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')
+	"$#{a}.#{b}"
+end
+
+def pie_chart_prep ary_of_hsh, hsh_values, label, new_ary, theme_color
+	new_ary = ary_of_hsh.map {|hsh| {value: hsh[hsh_values].to_f, label: "#{hsh[label]}"}}
+	new_ary = ary_to_hex_ary(theme_color, new_ary)
+	return new_ary
 end
 
 theme_color = '#60BBB2'
@@ -47,12 +57,6 @@ def darken_color(hex_color, amount)
   rgb[1] = (rgb[1].to_i * amount).round
   rgb[2] = (rgb[2].to_i * amount).round
   "#%02x%02x%02x" % rgb
-end
-
-def formatted_number(n)
-	a,b = sprintf("%0.2f", n).split('.')
-	a.gsub!(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')
-	"$#{a}.#{b}"
 end
 
 def lighten_color(hex_color, amount)
@@ -74,12 +78,6 @@ def ary_to_hex_ary (hex, ary)
     new_ary.push(hsh)
   end
   new_ary
-end
-
-def pie_chart_prep ary_of_hsh, hsh_values, label, new_ary, theme_color
-	new_ary = ary_of_hsh.map {|hsh| {value: hsh[hsh_values].to_f, label: "#{hsh[label]}"}}
-	new_ary = ary_to_hex_ary(theme_color, new_ary)
-	return new_ary
 end
 
 
@@ -131,18 +129,22 @@ records.each do |hsh|
     pilot_new["Delivery Count"] = records
       .select {|records| records["Pilot"] == hsh["Pilot"]}
       .map {|records| records["Pilot"]}
-      .count
+      .count.to_f
     pilot_ary << pilot_new
   end
 end
-pilot_ary = pilot_ary.sort{|x,y| x["Bonus Total"].to_i <=> y["Bonus Total"].to_i}
-# puts pilot_ary.inspect
-# pilot_ary.each do |hsh|
-#   puts "'string', '#{hsh[:name]}'"
-# end
-# pilot_ary.each do |hsh|
-#   puts hsh.values.inspect
-# end
+pilot_ary = pilot_ary.sort{|x,y| x["Bonus Total"].to_f <=> y["Bonus Total"].to_f}
+
+# Get data for Employee Summary table
+emp_sum_add_column = pilot_ary.last
+emp_sum_add_column = emp_sum_add_column
+	.map {|hsh| hsh.to_a}
+	.map {|key, value| "'#{value.class.to_s.downcase.gsub('float', 'number')}', '#{key}'"}
+
+emp_sum_add_rows = pilot_ary.map do |hsh|
+	hsh.values
+end
+
 
 pilot_pie_title = "Sales by Pilot"
 pilot_pie = pie_chart_prep(pilot_ary, "Delivery Total", "Pilot ", [], theme_color)
@@ -169,31 +171,14 @@ records.each do |hsh|
     planet_new["Delivery Count"] = records
       .select {|records| records["Destination"] == hsh["Destination"]}
       .map {|records| records["Pilot"]}
-      .count
+      .count.to_f
     planet_ary << planet_new
   end
 end
 planet_ary = planet_ary.sort{|x,y| x["Bonus Total"].to_i <=> y["Bonus Total"].to_i}
-# puts planet_ary.inspect
-# planet_ary.each do |hsh|
-#   puts "'string', '#{hsh[:name]}'"
-# end
-# planet_ary.each do |hsh|
-#   puts hsh.values.inspect
-# end
 
 planet_pie_title = "Sales by Planet"
 planet_pie = pie_chart_prep(planet_ary, "Delivery Total", "Destination ", [], theme_color)
-
-# format dollar values
-records.each do |hsh|
-  hsh["Money"] = formatted_number(hsh["Money"]) if hsh.has_key?("Money")
-  hsh["Bonus"] = formatted_number(hsh["Bonus"]) if hsh.has_key?("Bonus")
-end
-pilot_ary.each do |hsh|
-  hsh["Delivery Total"] = formatted_number(hsh["Delivery Total"]) if hsh.has_key?("Delivery Total")
-  hsh["Bonus Total"] = formatted_number(hsh["Bonus Total"]) if hsh.has_key?("Bonus Total")
-end
 
 
 # ==================== replace
